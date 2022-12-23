7cc779a8cad2442c95274231bf65a53998f89f2e
Van Darkholme <jija@kaavo>
2022-12-24 02:55:00 +0500

    Add project files.

---
+++ MusicDownloader/Models/Downloader.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace MusicDownloader.Models
+{
+    public class Downloader
+    {
+        private string _downloadsFolder { get; set; } = "/app/music/";
+
+        public Downloader()
+        {
+
+        }
+
+        public void Download(string link)
+        {
+            string path = _downloadsFolder + link + ".txt";
+            _ = File.Create(path);
+        }
+    }
+}
---
+++ MusicDownloader/Rabbit/RabbitMqListener.cs
@@ -0,0 +1,91 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+using MusicDownloader.Models;
+
+namespace MusicDownloader.Rabbit
+{
+    public class RabbitMqListener : BackgroundService
+    {
+        private IConnection _connection;
+        private IModel _channel;
+        private string _requestQueueName = "MusicDownloadRequests";
+        private string _resultsQueueName = "MusicDownloadResults";
+
+        public RabbitMqListener()
+        {
+            Console.WriteLine("CREATING CONNECTION");
+            var factory = GetConnectionFactory();
+            _connection = factory.CreateConnection();
+            Console.WriteLine("CONNECTION CREATED");
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: _requestQueueName, durable: false, 
+                exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+                Console.WriteLine("RECEIVED MESSAGE " + content);
+                Console.WriteLine("DOWNLOADING MUSIC " + content);
+                DownloadMusic(content);
+                Console.WriteLine("SENDING MESSAGE " + content);
+                SendMessage(content);
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume(_requestQueueName, false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        public override void Dispose()
+        {
+            _channel.Close();
+            _connection.Close();
+            base.Dispose();
+        }
+
+        private void DownloadMusic(string link)
+        {
+            Downloader downloader = new();
+            downloader.Download(link);
+        }
+
+        private void SendMessage(string message)
+        {
+            var factory = GetConnectionFactory();
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: _resultsQueueName,
+                           durable: false,
+                           exclusive: false,
+                           autoDelete: false,
+                           arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                           routingKey: _resultsQueueName,
+                           basicProperties: null,
+                           body: body);
+        }
+
+        private ConnectionFactory GetConnectionFactory()
+        {
+            return new ConnectionFactory
+            {
+                UserName = "bykrabbit",
+                Password = "bykbykbyk",
+                VirtualHost = "/",
+                HostName = "rabbit",
+                Port = AmqpTcpEndpoint.UseDefaultPort
+            };
+        }
+    }
+}
